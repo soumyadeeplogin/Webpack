b330ff0978882ce7f7896bc9bd512add87881245
soumyadeeplogin <soumyadeeplogin@gmail.com>
2018-07-05 16:47:57 +0530

    L362

---
+++ src/components/PizzaImage/PizzaImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './PizzaImage.css'
+
+import PizzaImage from '../../assests/pizza.jpg'
+const pizzaImage = (props) => (
+    <div calssName={classes.PizzaImage}>
+        <img src={PizzaImage} className={classes.PizzaImg} />
+    </div>
+)
+
+export default pizzaImage
---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React,  { Component} from 'react'
+
+class Users extends Component {
+    render () {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>Awesome users onboard</p>
+            </div>
+        )
+    }
+}
+
+
+export default Pizza
